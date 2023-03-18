70de960ebe4fcec59966e6906b5b1b5d6cab3bbc
dinith72 <jdinith72@gmail.com>
2023-03-18 12:04:25 +0530

    added roman to int logic

--- romantoInt.py
+++ romantoInt.py
@@ -1,23 +1,25 @@
-def RomanToInt(romanNumber) -> int :
+# problem link -
+
+def RomanToInt(romanNumber) -> int:
 
     lettersDict = {
-        'I': 1, 'V': 5, 'X': 10 ,'L':50, 'C':100, 'D':500, 'M':1000
-    }   
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
     number = 0
 
-    for i in range(0,len(romanNumber)):
+    for i in range(0, len(romanNumber)):
         letter = romanNumber[i].capitalize()
         letterVal = int(lettersDict[letter])
 
         nextLetterVal = 0
-        if(i < len(romanNumber)-1):
+        if (i < len(romanNumber)-1):
             nextLetter = romanNumber[i+1].capitalize()
-            nextLetterVal = int(lettersDict[nextLetter]) 
-                       
-        if(letterVal < nextLetterVal and nextLetterVal != 0):
-            number -= letterVal 
+            nextLetterVal = int(lettersDict[nextLetter])
+
+        if (letterVal < nextLetterVal and nextLetterVal != 0):
+            number -= letterVal
         else:
-         number +=  letterVal
+            number += letterVal
 
     return number
 
