ed2d9e2cfa8ac6b8c4d892b811b29fcde61a7483
Dinith Jayabodhi <Dinith.Jayabodhi@cube.global>
2023-04-05 07:23:59 +0530

    added .net project

---
+++ DotNet/LeetCodeSolutions/LeetCodeSolutions/Program.cs
@@ -0,0 +1,15 @@
+﻿using LeetCodeSolutions.mediumProblems;
+
+namespace LeetCodeSolutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /// medium level challenges  
+
+            new RomanToInt().IntToRoman(1994);
+
+        }
+    }
+}
