8461814bb2843c5d0895cae44915c8c53aa6bffc
dinith72 <jdinith72@gmail.com>
2024-03-10 06:40:24 +0530

    added palidrome and two sum

---
+++ DotNet/LeetCodeSolutions/LeetCodeSolutions/easyProblems/Palindrome.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCodeSolutions.easyProblems;
+
+// problem: https://leetcode.com/problems/palindrome-number/
+class Palindrome
+{
+    // average on the memory side and the runtime 
+    public bool IsPalindrome(int x)
+    {
+        // negative numbers are not palindrome because of the - sign
+        if(x < 0)
+            return false;   
+
+        var numStr = x.ToString();
+        int midPoint =  numStr.Length / 2 ; 
+
+        for (int i = 0; i < midPoint; i++)
+        {
+            if (numStr[i] != numStr[numStr.Length - 1 - i])
+                return false;
+        }
+        return true;
+    }
+
+    // this is taken from the number reversal
+    // performs well on the memory side 
+    public bool isPalindrome2(int x)
+    {
+        // numbers less than 0 are not palindrome because of the - sign
+        if(x < 0)
+            return false;
+
+        int temp = x;
+        int reversed = 0;
+
+        while(temp > 0)
+        {
+            var lastDigit = temp % 10;
+            temp = temp / 10;
+            reversed = reversed * 10 + lastDigit; 
+        }
+
+        return reversed == x;
+    }
+}
---
+++ DotNet/LeetCodeSolutions/LeetCodeSolutions/easyProblems/TwoSum.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodeSolutions.easyProblems;
+
+// problem: https://leetcode.com/problems/two-sum/
+class TwoSum
+{
+    public int[] GetTwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var diff = target - nums[i];
+            var diffIndex = Array.IndexOf(nums, diff);
+
+            if(diffIndex != -1 && diffIndex != i)
+                return new int[] { i, diffIndex };
+            
+        }
+        return new int[] { 0, 0 };
+    }
+}
