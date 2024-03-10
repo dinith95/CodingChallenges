7687becd24f7008f77f4e5bf81c652500f09b2c0
dinith72 <jdinith72@gmail.com>
2024-03-10 20:58:49 +0530

    added add two numbers and the longest palindrome substring

---
+++ DotNet/LeetCodeSolutions/LeetCodeSolutions/mediumProblems/AddTwoNUmbers.cs
@@ -0,0 +1,73 @@
+﻿namespace LeetCodeSolutions.mediumProblems;
+
+// problem: https://leetcode.com/problems/add-two-numbers/
+class AddTwoNUmbers
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode SetUpLinkedLists(int value)
+    { 
+        var head = new ListNode(value % 10);
+        var temp = head;
+        value = value / 10;
+        while(value >= 1)
+        {
+            var node = new ListNode(value % 10);
+            value = value / 10;
+            temp.next = node;
+            temp = node;
+        }
+        return head;
+    }
+
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode resultHead = new ListNode();
+        ListNode resultTemp = resultHead;
+        int remainder = 0;
+        
+        // if both lists are empty stops the executions 
+        while(l1 != null || l2 != null || remainder > 0)
+        {
+            // get the sum of two list nodes and the remainder from the previous sum
+            int sum = (l1?.val ?? 0) + (l2?.val ?? 0) + remainder;
+            remainder = sum / 10;
+
+            
+            // addt the quotient to the result node
+            resultTemp.val = sum % 10;
+            if(l1?.next != null || l2?.next != null || remainder > 0)
+                resultTemp.next = new ListNode();
+
+            // move to the next node
+            resultTemp = resultTemp.next;
+            l1 = l1?.next;
+            l2 = l2?.next;
+        }
+       
+        return resultHead;
+    }
+
+
+    private int getNumber(ListNode l)
+    {
+        int num = 0;
+        int multiplier = 1;
+        while (l != null)
+        {
+            num += l.val * multiplier;
+            multiplier *= 10;
+            l = l.next;
+        }
+        return num;
+    }
+}
---
+++ DotNet/LeetCodeSolutions/LeetCodeSolutions/mediumProblems/LongestPalindromeSubstring.cs
@@ -0,0 +1,128 @@
+﻿using System.Net;
+
+namespace LeetCodeSolutions.mediumProblems;
+
+// https://leetcode.com/problems/longest-palindromic-substring/
+class LongestPalindromeSubstring
+{
+    private readonly object resultStrLock = new object();
+    public string LongestPalindrome(string s)
+    {
+        if(s.Length == 1)
+            return s;
+        if (s.Length == 2 )
+            return s[0] == s[1] ? s : s[0].ToString();
+
+        int point = s.Length / 2;
+        var resultStr = string.Empty;
+      
+        Task t1 = PropogateRight(s, point, ref resultStr);
+        Task t2 = ProppgateLeft(s, point, ref resultStr);
+       
+        Task.WaitAll(t1, t2);
+        return resultStr;
+    }
+
+    private Task PropogateRight(string s, int right, ref string resultStr)
+    {
+        // between the right and end ( index s.Length - 1 )  should has atleast half of the
+        // charaters in result string to be able to find a longer palindrome
+        while ((right + resultStr.Length / 2) < s.Length - 1)
+        {
+            var p1 = ExpandAroundIndex_OddPalindrome(s, right);
+
+            lock (resultStrLock)
+            {
+                if (p1.Length > resultStr.Length)
+                    resultStr = p1;
+            }
+           
+            // check for even palindrome
+            if (right + 1 < s.Length && s[right] == s[right + 1])
+            {
+                var p2 = ExpandAroundIndex_EvenPalindrome(s, right, right + 1);
+                
+                lock (resultStrLock)
+                {
+                    if (p2.Length > resultStr.Length)
+                        resultStr = p2;
+                }         
+            }
+            right += 1;
+        }
+        return Task.CompletedTask;
+    }
+
+    private Task ProppgateLeft(string s, int left, ref string resultStr)
+    {
+        // between the left and start ( index 0 )  should has atleast half of the
+        // charaters in result string to be able to find a longer palindrome
+        while ((left - resultStr.Length / 2) >= 0)
+        {
+            var p1 = ExpandAroundIndex_OddPalindrome(s, left);
+
+            lock (resultStrLock)
+            {
+                if (p1.Length > resultStr.Length)
+                    resultStr = p1;
+            }
+         
+            // check for even palindrome
+            if (left > 0 && s[left] == s[left - 1])
+            {
+                var p2 = ExpandAroundIndex_EvenPalindrome(s, left - 1, left);
+                lock (resultStrLock)
+                {
+                    if (p2.Length > resultStr.Length)
+                        resultStr = p2;
+                }         
+            }
+            left -= 1;
+        }
+        return Task.CompletedTask;
+    }
+
+    private string ExpandAroundIndex_OddPalindrome(string s, int pos)
+    {
+        bool isPalindrome = true;
+        int left = pos;
+        int right = pos;
+        while (isPalindrome && left > 0 && right +1 < s.Length)
+        {
+            left -= 1;
+            right += 1;
+            isPalindrome &= s[left] == s[right];
+
+            // if palidrome is not valid reset the left and right
+            if (!isPalindrome)
+            {
+                left += 1;
+                right -= 1;
+            }
+        }
+
+        // adding 1 since converting number of characters from index
+        return s.Substring(left , right + 1 - left) ; 
+    }
+
+    private string ExpandAroundIndex_EvenPalindrome(string s, int left, int right)
+    {
+        bool isPalindrome = true;
+         
+        while (isPalindrome && left - 1 >= 0 && right + 1 < s.Length)
+        {     
+            left -= 1;
+            right += 1;
+            isPalindrome &= s[left] == s[right];
+            // if palidrome is not valid reset the left and right
+            if (!isPalindrome)
+            {
+                left += 1;
+                right -= 1;
+            }
+        }
+
+        // adding 1 since converting number of characters from index
+        return s.Substring(left , right + 1 - left);
+    }   
+}
