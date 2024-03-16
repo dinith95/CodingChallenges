92ccded3a8412b29a2adca6754863f9b2725350f
Dinith Jayabodhi <Dinith.Jayabodhi@cube.global>
2024-03-16 07:50:16 +0530

    added two problems

---
+++ DotNet/LeetCodeSolutions/LeetCodeSolutions/mediumProblems/CustomSortString.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCodeSolutions.mediumProblems;
+
+// problem: https://leetcode.com/problems/custom-sort-string/
+class CustomSortString
+{
+    public string GetCustomSortString(string order, string str)
+    {
+        Dictionary<char, int> orderMap = new Dictionary<char, int>();
+        var result = string.Empty;
+
+        // create ampping of the character and its counts in the string 
+        foreach (var c in str)
+        {
+            // if the character is there increase the count 
+           if(orderMap.ContainsKey(c))
+                orderMap[c] += 1;
+           // if the character is not there add it to the map
+           else
+               orderMap.Add(c, 1);
+        }
+
+        // iterate through the order string and add the characters to the result string
+        foreach (var c in order)
+        {
+            if (orderMap.ContainsKey(c))
+            {
+                int count = orderMap[c];
+
+                for (int i = 0; i < count; i++)
+                    result += c;
+
+                orderMap.Remove(c); // remove the character from the map as it is already processed
+            }
+        }
+
+        // add the remaining characters to the result string
+        foreach (var character in orderMap)
+        {
+            for (int i = 0; i < character.Value; i++)
+                result += character.Key;
+        }
+
+        return result;
+    }   
+}
---
+++ DotNet/LeetCodeSolutions/LeetCodeSolutions/mediumProblems/ReverseInteger.cs
@@ -0,0 +1,29 @@
+﻿using System.Dynamic;
+
+namespace LeetCodeSolutions.mediumProblems;
+
+// https://leetcode.com/problems/reverse-integer/description/
+class ReverseInteger
+{
+    public int GetReverseInt(int x)
+    {
+        if(x >= int.MaxValue || x <= int.MinValue)
+            return 0;
+
+        int num = Math.Abs(x);
+        int result = 0;
+
+        while(num >= 1)
+        {
+            var rem = num % 10;
+            num = num / 10;
+
+            if((int.MaxValue - rem)/10 < result)
+                return 0;
+
+            result = result * 10 + rem;          
+        }
+
+        return x < 0 ? -result : result;
+    }
+}
