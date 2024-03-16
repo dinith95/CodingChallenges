4f45d5914187fb773b6095b70bc9bfba38165fe7
dinith72 <jdinith72@gmail.com>
2024-03-16 18:55:33 +0530

    added the container with more water and three sum

---
+++ DotNet/LeetCodeSolutions/LeetCodeSolutions/mediumProblems/ContainerWithMostWater.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeSolutions.mediumProblems;
+
+class ContainerWithMostWater
+{
+
+    public int MaxArea(int[] height)
+    {
+        int left = 0;
+        int right = height.Length - 1;
+        int maxArea = 0;
+
+        while (left < right)
+        {
+            int area = height[left] < height[right] ?
+                height[left] * (right - left) : height[right] * (right - left);
+           
+            if(area > maxArea)
+                maxArea = area;
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return maxArea;
+    }
+}
---
+++ DotNet/LeetCodeSolutions/LeetCodeSolutions/mediumProblems/ThreeSum.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeSolutions.mediumProblems;
+
+class ThreeSum
+{
+    record TwoSumResult(int num1, int num2);
+    public IList<IList<int>> GetThreeSum(int[] nums)
+    {
+        var result = new List<IList<int>>();
+
+        var twoSumDict = getTwoSumDict(nums);
+
+        foreach (var n in nums)
+        {
+            if(twoSumDict.ContainsKey(-n))
+            {
+                var twoSum = twoSumDict[-n];
+                result.Add(new List<int> { n , twoSum.num1, twoSum.num2});
+                twoSumDict.Remove(-n);
+            }
+        }
+
+        return result;
+    }
+
+
+    private Dictionary<int, TwoSumResult> getTwoSumDict(int[] nums)
+    {
+        var twoSumDict = new Dictionary<int, TwoSumResult>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                var sum = nums[i] + nums[j];
+                if (!twoSumDict.ContainsKey(sum))
+                    twoSumDict.Add(sum, new TwoSumResult(nums[i], nums[j]));       
+            }
+        }
+        return twoSumDict;
+    }
+}
