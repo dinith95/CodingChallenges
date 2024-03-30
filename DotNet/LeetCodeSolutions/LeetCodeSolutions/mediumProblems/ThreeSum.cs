98043a69c36affeacd8c37b6c9e5411e409635a2
Dinith Jayabodhi <Dinith.Jayabodhi@cube.global>
2024-03-30 07:40:11 +0530

    updated the three sum logic

--- DotNet/LeetCodeSolutions/LeetCodeSolutions/mediumProblems/ThreeSum.cs
+++ DotNet/LeetCodeSolutions/LeetCodeSolutions/mediumProblems/ThreeSum.cs
@@ -2,37 +2,60 @@
 
 class ThreeSum
 {
-    record TwoSumResult(int num1, int num2);
+    record TwoSumResult(int pos1, int pos2);
     public IList<IList<int>> GetThreeSum(int[] nums)
     {
-        var result = new List<IList<int>>();
+        var resultDict = new Dictionary<string,IList<int>>();
 
         var twoSumDict = getTwoSumDict(nums);
 
-        foreach (var n in nums)
+        for (int i =0; i < nums.Length; i++)
         {
-            if(twoSumDict.ContainsKey(-n))
+            var n = nums[i];
+            if (twoSumDict.ContainsKey(-n))
             {
-                var twoSum = twoSumDict[-n];
-                result.Add(new List<int> { n , twoSum.num1, twoSum.num2});
-                twoSumDict.Remove(-n);
+                var twoSumList = twoSumDict[-n];
+                foreach (var item in twoSumList)
+                    ProcessTwoSumMatch(item,nums, resultDict, i);
             }
         }
 
-        return result;
+      return resultDict.Values.ToList();
+       
+    }
+
+    private void ProcessTwoSumMatch(TwoSumResult twoSum,int[] nums, Dictionary<string, IList<int>> resultDict, int i)
+    {
+        if (i == twoSum.pos1 || i == twoSum.pos2)
+            return;
+
+        var resultList = new List<int> { nums[i], nums[twoSum.pos1], nums[twoSum.pos2] };
+        resultList.Sort();
+        var indexstr = string.Join("~", resultList);
+
+        if (!resultDict.ContainsKey(indexstr))
+            resultDict.Add(indexstr, resultList);
     }
 
 
-    private Dictionary<int, TwoSumResult> getTwoSumDict(int[] nums)
+    private Dictionary<int, List<TwoSumResult>> getTwoSumDict(int[] nums)
     {
-        var twoSumDict = new Dictionary<int, TwoSumResult>();
+        var twoSumDict = new Dictionary<int, List<TwoSumResult>>();
         for (int i = 0; i < nums.Length; i++)
         {
             for (int j = i + 1; j < nums.Length; j++)
             {
                 var sum = nums[i] + nums[j];
                 if (!twoSumDict.ContainsKey(sum))
-                    twoSumDict.Add(sum, new TwoSumResult(nums[i], nums[j]));       
+                {
+                    var list = new List<TwoSumResult>() { new TwoSumResult(i, j) };
+                    twoSumDict.Add(sum, list);
+                }
+                else
+                {
+                    var list = twoSumDict[sum];
+                    list.Add(new TwoSumResult(i, j));
+                }
             }
         }
         return twoSumDict;
